cb8b41ce354ee75b8d54df00ced1c715a0593788
Josh <joshconstantinelim@gmail.com>
2023-06-02 00:40:46 +1000

    delete image

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -5,14 +5,6 @@ const Register = () => {
   return (
     <main className="fixed w-full h-full flex items-center justify-center bg-white p-5">
       <div className="shadow-2xl xl:w-5/12 lg:w-8/12 md:flex-row flex-col max-w-4xl text-[#333] rounded-2xl flex overflow-hidden">
-        {/* <Image
-          src="https://ichef.bbci.co.uk/news/976/cpsprodpb/D2D2/production/_118707935_untitled-1.jpg"
-          alt="pic"
-          height={100}
-          width={400}
-          priority
-          style={{ width: "auto" }}
-        /> */}
         <div className="md:w-6/12 flex justify-center items-center flex-col py-10 md:p-0 bg-light-rainbow">
           <Image
             src="/logo_greyscale.jpeg"
