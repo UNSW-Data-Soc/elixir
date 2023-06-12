93a9a245a3127d10fb01ba3144534ddbee550bac
Adrian Lim <adrian.lim125@gmail.com>
2023-06-12 22:34:31 +1000

    admin setup

---
+++ src/app/sponsorships/page.tsx
@@ -0,0 +1,16 @@
+export default function Sponsorships() {
+  return (
+    <main className="bg-white ">
+      <header className="text-white p-12 bg-[#4799d1] flex flex-col gap-4">
+        <h1 className="text-3xl font-semibold">Sponsorships</h1>
+        <p>
+          TODO: Get the description from the old website!
+        </p>
+      </header>
+      <button style={{ backgroundColor: "red" }}>
+        Add Sponsor (TODO: change this button to a clickable "+" card, only visible to mods/admins)
+      </button>
+      <p>TODO: Dialog for <b>company</b> go here!</p>
+    </main>
+  );
+}
