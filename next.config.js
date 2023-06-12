86382867981fe49cae6e6e9b96ffeb5ea326beb4
Josh <joshconstantinelim@gmail.com>
2023-06-12 14:44:50 +1000

    remove unused nextconfig image domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { domains: ["ichef.bbci.co.uk"] },
+  // images: { domains: ["ichef.bbci.co.uk"] },
 };
 
 module.exports = nextConfig;
