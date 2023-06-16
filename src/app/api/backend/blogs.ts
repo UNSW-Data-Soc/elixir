9156aecf4d2758a6754ac440be1a4378ecc7993f
Josh <joshconstantinelim@gmail.com>
2023-06-17 00:45:11 +1000

    add blogs type

--- src/app/api/backend/blogs.ts
+++ src/app/api/backend/blogs.ts
@@ -1,11 +1,22 @@
 import { callFetch } from "./endpoints";
 
-const getAll = async () => {
-  return await callFetch({
+export interface Blog {
+  creator: string;
+  title: string;
+  body: string;
+  author: string;
+  public: true;
+  id: string;
+  created_time: "2023-06-16T10:59:09.059Z";
+  last_edit_time: "2023-06-16T10:59:09.059Z";
+}
+
+const getAll: () => Promise<Blog[]> = async () => {
+  return (await callFetch({
     route: "/blogs",
     method: "GET",
     authRequired: false,
-  });
+  })) as Blog[];
 };
 
 export const blogs = {
