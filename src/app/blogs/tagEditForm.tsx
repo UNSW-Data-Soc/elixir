ebfcc09dac5596586189f91e177169b8cf5ab497
Dulini Galhena <dulinigalhena@dulinis-air.home>
2023-06-16 18:28:41 +1000

    added drop bar for choosign colour modification

--- src/app/blogs/tagEditForm.tsx
+++ src/app/blogs/tagEditForm.tsx
@@ -3,6 +3,14 @@ import React, { useState } from "react";
 const TagEditForm = ({ tag, onSave }) => {
   const [name, setName] = useState(tag.name);
   const [color, setColor] = useState(tag.color);
+  const colorOptions = [
+    { label: 'Red', value: 'red' },
+    { label: 'Blue', value: 'blue' },
+    { label: 'Green', value: 'green' },
+    { label: 'Yellow', value: 'yellow' },
+    { label: 'Orange', value: 'orange' },
+    { label: 'Purple', value: 'purple' },
+  ];
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -28,15 +36,25 @@ const TagEditForm = ({ tag, onSave }) => {
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
-      </div>
+      </div>  
       <div>
         <label htmlFor="color">Color: </label>
-        <input
-          type="text"
+        <select
           id="color"
           value={color}
           onChange={(e) => setColor(e.target.value)}
-        />
+        >
+          <option value="">Select a color</option>
+          {colorOptions.map((option) => (
+            <option
+              key={option.value}
+              value={option.value}
+              style={{ backgroundColor: option.value }}
+            >
+              {option.label}
+            </option>
+          ))}
+        </select>
       </div>
       <button type="submit">Save</button>
     </form>
