c1f90e034843a2a2b19b9e20896354e8efe10434
Josh <joshconstantinelim@gmail.com>
2023-06-17 01:14:04 +1000

    add post page

--- src/app/blogs/post/page.tsx
+++ src/app/blogs/post/page.tsx
@@ -1,15 +1,30 @@
 "use client";
 
 import { useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { FormEventHandler } from "react";
 
 export default async function BlogsPost() {
-  const session = useSession();
-  if (session.status === "unauthenticated" || !session.data?.user.admin) redirect("/blogs");
+  // const session = useSession();
+  // const router = useRouter();
+  // if (session.status === "unauthenticated" || !session.data?.user.admin) router.push("/blogs");
+
+  const handleFormSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+
+    console.log("FORM SUBMITTED");
+  };
 
   return (
     <main className="p-24 text-black">
       <h1 className="text-3xl font-bold">Create a new blog post.</h1>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="submit"
+          value="All done!"
+          className="px-5 py-3 rounded-xl shadow-lg bg-sky-500 cursor-pointer hover:bg-sky-600 transition-colors"
+        />
+      </form>
     </main>
   );
 }
