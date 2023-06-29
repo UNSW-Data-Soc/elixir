ef8210d57a97edef88c3770012b57e96a3569b94
Lucas Sutherland <sutherlandlucas1@gmail.com>
2023-06-30 06:19:36 +1000

    yearh

---
+++ src/app/api/backend/events.ts
@@ -0,0 +1,48 @@
+import { randomUUID } from "crypto";
+import { callFetch } from "./endpoints";
+
+export interface Event {
+  creator: string;
+  title: string;
+  description: string;
+  start_date: "2023-06-29T10:46:26.753Z";
+  end_date: "2023-06-29T10:46:26.753Z";
+  location: string;
+  link: string;
+  id: string;
+  photo_id: string;
+  last_edit_time: "2023-06-29T10:46:26.753Z"
+}
+
+const getAll: () => Promise<Event[]> = async () => {
+  return (await callFetch({
+    route: "/events",
+    method: "GET",
+    authRequired: false,
+  })) as Event[];
+};
+
+interface CreateEvent {
+  photo: string;
+  creator: string;
+  title: string;
+  description: string;
+  start_date: "2023-06-29T10:46:26.753Z";
+  end_date: "2023-06-29T10:46:26.753Z";
+  location: string;
+  link: string;
+}
+
+const create: (event: CreateEvent) => Promise<Event> = async (event: CreateEvent) => {
+  return await callFetch({
+    method: "POST",
+    route: "/event",
+    authRequired: true,
+    body: JSON.stringify({ ...event }),
+  });
+};
+
+export const events = {
+  getAll,
+  create,
+};
---
+++ src/app/events/eventsAddButton.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { PlusIcon } from "@heroicons/react/24/outline";
+
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+export default function EventsAddButton() {
+    const session = useSession();
+    const router = useRouter();
+
+    if (session.status === "authenticated" && session.data.user.admin) {
+        return (
+            <button
+                className="transition-all bg-[#4799d1] fixed bottom-7 right-7 z-50 rounded-full p-3 shadow-xl hover:bg-[#267ab1]"
+                onClick={() => router.push("/events/post")}
+            >
+                <PlusIcon className="h-12 w-12 text-white" />
+            </button>
+        );
+    }
+
+    return <></>;
+}
