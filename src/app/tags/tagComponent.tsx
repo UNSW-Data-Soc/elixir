105b03a4dd63f2439fda93dc53c72f79ead3092e
dulini.galhena <sedaragalhena@gmail.com>
2023-07-12 22:15:59 +1000

    removing react-select - not installed properly

--- src/app/tags/tagComponent.tsx
+++ src/app/tags/tagComponent.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useEffect, useState } from 'react';
+import Select from 'react-select';
 import { endpoints } from "../api/backend/endpoints";
 import { Tag } from "../api/backend/tags";
 import TagForm from "./tagForm";
