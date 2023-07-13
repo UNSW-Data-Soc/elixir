d8fa009673365045b3f6e53ff3cbe6fe8053d052
dulini.galhena <sedaragalhena@gmail.com>
2023-07-13 14:37:02 +1000

    added pop up dialogue for editing tags

--- src/app/tags/tagEditForm.tsx
+++ src/app/tags/tagEditForm.tsx
@@ -1,15 +1,24 @@
+"use client";
 import React, { useState } from "react";
+import Select from "react-select";
+import { Tag } from "../api/backend/tags";
 
 const TagEditForm = ({ tag, onSave }) => {
   const [name, setName] = useState(tag.name);
   const [color, setColor] = useState(tag.color);
+
   const colorOptions = [
-    { label: 'Red', value: 'red' },
-    { label: 'Blue', value: 'blue' },
-    { label: 'Green', value: 'green' },
-    { label: 'Yellow', value: 'yellow' },
-    { label: 'Orange', value: 'orange' },
-    { label: 'Purple', value: 'purple' },
+    { label: 'Dark Blue', value: '#159BD6' },
+    { label: 'Dark Purple', value: '#9E62A2' },
+    { label: 'Red', value: '#E04552' },
+    { label: 'Orange', value: '#EA6D38' },
+    { label: 'Yellow', value: '#F5B102' },
+    { label: 'Light Blue', value: '#98CEEA' },
+    { label: 'Lavender', value: '#D0B0D0' },
+    { label: 'Pink', value: '#F2A5A9' },
+    { label: 'Coral', value: '#F5B89E' },
+    { label: 'Light', value: '#FBD896' },
+
   ];
 
   const handleSubmit = (e) => {
@@ -26,6 +35,10 @@ const TagEditForm = ({ tag, onSave }) => {
     setColor("");
   };
 
+  const handleColorChange = (selectedOption) => {
+    setColor(selectedOption.value);
+  };
+
   return (
     <form onSubmit={handleSubmit}>
       <div>
@@ -36,29 +49,22 @@ const TagEditForm = ({ tag, onSave }) => {
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
-      </div>  
+      </div>
       <div>
         <label htmlFor="color">Color: </label>
-        <select
+        <Select
+          options={colorOptions}
+          classNamePrefix="select"
+          isSearchable={true}
           id="color"
-          value={color}
-          onChange={(e) => setColor(e.target.value)}
-        >
-          <option value="">Select a color</option>
-          {colorOptions.map((option) => (
-            <option
-              key={option.value}
-              value={option.value}
-              style={{ backgroundColor: option.value }}
-            >
-              {option.label}
-            </option>
-          ))}
-        </select>
+          value={colorOptions.find((option) => option.value === color)}
+          onChange={handleColorChange}
+        />
       </div>
       <button type="submit">Save</button>
     </form>
   );
 };
+
 
 export default TagEditForm;
--- src/app/tags/tagItem.tsx
+++ src/app/tags/tagItem.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Tag } from '../api/backend/tags';
+import TagEditForm from './TagEditForm';
 
 interface TagItemProps {
   tag: Tag;
 }
+
 const TagItem: React.FC<TagItemProps> = ({ tag }) => {
+  const [showEditForm, setShowEditForm] = useState(false);
+  const [editedTag, setEditedTag] = useState(tag);
+
+  const handleTagClick = () => {
+    setShowEditForm(true);
+  };
+
+  const handleSave = (updatedTag: Tag) => {
+    setEditedTag(updatedTag); // Update the edited tag
+    setShowEditForm(false);
+  };
+
+  const handleCancelEdit = () => {
+    setShowEditForm(false);
+  };
+
   const tagStyle = {
     backgroundColor: tag.colour,
     color: '#ffffff',
@@ -14,10 +32,37 @@ const TagItem: React.FC<TagItemProps> = ({ tag }) => {
     display: 'inline-block',
     whiteSpace: 'nowrap',
     width: 'auto',
-    fontSize: '10px',
+    fontSize: '12px', // Adjust the font size as needed
+    cursor: 'pointer',
+    position: 'relative',
+    // Explicitly set the background color
+    background: tag.colour,
   };
 
-  return <div style={tagStyle}>{tag.name}</div>;
-};
+  const popupStyle = {
+    position: 'absolute',
+    top: 'calc(100% + 5px)', // Adjust the distance between the tag and the popup as needed
+    left: '0',
+    zIndex: '9999',
+    backgroundColor: '#ffffff',
+    padding: '10px',
+    borderRadius: '4px',
+    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
+  };
+
+  return (
+    <div style={{ position: 'relative' }}>
+      <div style={tagStyle} onClick={handleTagClick}>
+        {tag.name}
+      </div>
+      {showEditForm && (
+        <div style={popupStyle}>
+          <TagEditForm tag={editedTag} onSave={handleSave} />
+          <button onClick={handleCancelEdit}>Cancel</button>
+        </div>
+      )}
+    </div>
+  );
+}
 
 export default TagItem;
