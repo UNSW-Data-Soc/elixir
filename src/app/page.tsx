03e3fca9141b2e5b5a1af7b79ded3b5c72488fa7
Prayag <prayag1601@gmail.com>
2023-08-30 01:56:09 +1000

    optimise cover photo by using Image component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,32 @@
 import Image from "next/image";
-// import { Image} from "@nextui-org/react";
 import { endpoints } from "./api/backend/endpoints";
-import { COVER_PHOTO_X_PXL, COVER_PHOTO_Y_PXL } from "./utils";
+
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-center bg-light-rainbow gap-10 left-0 top-0 w-full z-0 select-none pb-48">
-            <div style={{
-                backgroundImage: `url(${endpoints.file.getCoverPhoto()})`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                overflow: "hidden",
-                height: "100vh",
-                width: "100vw",
-            }}
-            className="fade-in-image mix-blend-multiply"
-            >
-                <div className="pl-48 pt-48 flex flex-col items-start justify-center align-baseline gap-3 text-white">
-                    <div className="text-3xl font-bold">
-                        Welcome to the
-                    </div>
-                    <div className="text-7xl font-bold">
-                        Data Science Society
-                    </div>
-                    <div className="text-3xl font-bold">
-                        University of New South Wales
-                    </div>
+        <main className="min-h-screen bg-light-rainbow gap-10 left-0 top-0 w-full z-0 select-none pb-48">
+            <div className="fade-in-image mix-blend-multiply">
+                <div>
+                    <Image
+                        src={endpoints.file.getCoverPhoto()}
+                        alt="Cover Photo"
+                        fill
+                        sizes="100vw"
+                        style={{
+                            objectFit: "cover",
+                        }}
+                        priority
+                        quality={100}
+                    />
                 </div>
             </div>
-            <div className="flex items-center justify-center align-baseline text-white text-2xl">
+            <div className="brightness-200 pl-48 pt-48 flex flex-col items-start justify-center align-baseline gap-3 text-white">
+                <div className="text-3xl font-bold">Welcome to the</div>
+                <div className="text-7xl font-bold">Data Science Society</div>
+                <div className="text-3xl font-bold">
+                    University of New South Wales
+                </div>
             </div>
+            <div className="flex items-center justify-center align-baseline text-white text-2xl"></div>
         </main>
     );
 }
