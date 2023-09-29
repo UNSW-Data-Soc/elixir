c4b829d405e8051b86776800296d917e9dfbf6ff
wanning <z5371162@unsw.edu.au>
2023-09-29 23:03:07 +1000

    scaffolding copy

---
+++ src/app/about/page.tsx
@@ -0,0 +1,56 @@
+export default function About() {
+  return (
+    <main className='bg-white '>
+      <header className='text-white p-12 bg-[#4799d1] flex flex-col gap-4'>
+        <h1 className='text-3xl font-semibold'>About Us</h1>
+        <p>
+          What we really do. Learn about the society that continually seeks the
+          best for students.
+        </p>
+      </header>
+      <p>
+        Uniting mathematicians, econometricians and computer scientists, UNSW
+        DataSoc seeks to empower our members with knowledge and skills of data
+        science, machine learning and artificial intelligence.
+      </p>
+      <h1 className='text-3xl font-semibold'>Affiliated with</h1>
+      <h1 className='text-3xl font-semibold'>Our Goals</h1>
+      <ul>
+        <li>
+          DataSoc aims to become Australia’s leading student run society
+          platform in assisting students on achieving their data science career
+          goals.
+        </li>
+        <li>
+          We strive to create the data science related opportunities for
+          students in their studies and careers alike. We host information
+          sessions, networking evenings, and many more career-focused events
+          that could help open new pathways for students.
+        </li>
+        <li>
+          We aim to enrich students' lives with a sense of community and
+          diversity among UNSW data science students. We host a list of various
+          social activities such as BBQs, competitions, etc. that welcome
+          everyone to attend and meet like minded people.
+        </li>
+        <li>
+          We want to support data science students in their studies with
+          DataSoc’s help sessions, workshops and peer supporters.
+        </li>
+      </ul>
+      <p>
+        With this, DataSoc aims to make your university experience even more fun
+        and fulfilling, whilst maximizing your employment opportunity and career
+        progression in data science.
+      </p>
+      <h2>"Opportunities don't happen. You create them."</h2>
+      <h1 className='text-3xl font-semibold'>Our Values</h1>
+      <p>
+        Our successes to date could not have be achieved without our thriving
+        subcommittee teams year after year. Here are our core values that
+        persists amongst all portfolios:
+      </p>
+      <h1 className='text-3xl font-semibold'>Our History</h1>
+    </main>
+  );
+}
--- src/app/sponsorships/page.tsx
+++ src/app/sponsorships/page.tsx
@@ -1,22 +1,21 @@
-import SponsorshipAddCard from "./SponsorshipAddCard";
-import SponsorshipsList from "./sponsorshipsList";
+import SponsorshipAddCard from './SponsorshipAddCard';
+import SponsorshipsList from './sponsorshipsList';
 
 export default function Sponsorship() {
-  
   return (
-      <main className="bg-white ">
-          <header className="text-white p-12 bg-[#4799d1] flex flex-col gap-4">
-          <h1 className='text-3xl font-semibold'>Our Sponsors</h1>
+    <main className='bg-white '>
+      <header className='text-white p-12 bg-[#4799d1] flex flex-col gap-4'>
+        <h1 className='text-3xl font-semibold'>Our Sponsors</h1>
         <p>
-          DataSoc wouldn&apos;t be DataSoc without the amazing companies we&apos;ve worked
-          with throughout our journey as a society. With more and more
-          data-oriented decisions and predictions made everyday, the demand for
-          talented Data Science graduates is growing.
+          DataSoc wouldn&apos;t be DataSoc without the amazing companies
+          we&apos;ve worked with throughout our journey as a society. With more
+          and more data-oriented decisions and predictions made everyday, the
+          demand for talented Data Science graduates is growing.
         </p>
         <p>
-          It&apos;s our long term goal to help our members become as capable they can
-          be, and it wouldn&apos;t be possible without the continued support from
-          industry.
+          It&apos;s our long term goal to help our members become as capable
+          they can be, and it wouldn&apos;t be possible without the continued
+          support from industry.
         </p>
         <p>
           <strong>
@@ -24,17 +23,17 @@ export default function Sponsorship() {
             <a href='mailto:external@unswdata.com'>external@unswdata.com</a>
           </strong>
         </p>
-          </header>
-          <SponsorshipsContainer />
-      </main>
+      </header>
+      <SponsorshipsContainer />
+    </main>
   );
 }
 
 function SponsorshipsContainer() {
-    return (
-        <div className="container m-auto flex flex-col gap-5 p-10 flex-wrap justify-center">
-            <SponsorshipAddCard/>
-            <SponsorshipsList/>
-        </div>
-    );
+  return (
+    <div className='container m-auto flex flex-col gap-5 p-10 flex-wrap justify-center'>
+      <SponsorshipAddCard />
+      <SponsorshipsList />
+    </div>
+  );
 }
