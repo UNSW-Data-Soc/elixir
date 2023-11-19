4e14ca37279f330841622ca68f82feffe2fa1d4a
Prayag <prayag1601@gmail.com>
2023-11-19 19:48:41 +1100

    update token maxage

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { getToken } from 'next-auth/jwt';
-import jwt from 'jsonwebtoken';
 
 
 // Limit the middleware to paths starting with `/api/`
@@ -22,17 +21,12 @@ export const config = {
 export async function middleware(req: NextRequest, res: NextResponse) {
   const token = await getToken({ req })
 
-
   if (!token) {
     return NextResponse.redirect(new URL('/auth/login', req.url));
   }
   if (token && token.exp) {
-    const decoded = jwt.decode(token.token);
-    const exp =  new Date(decoded.exp * 1000);
+    const exp =  new Date(token.exp as number* 1000);
     const currentTime = new Date();
-
-    console.log("expiry time:  " +  exp);
-    console.log("current time:  " + currentTime);
 
     if(exp < currentTime) {
       return NextResponse.redirect(new URL('/auth/login', req.url));
