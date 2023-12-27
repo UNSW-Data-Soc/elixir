bb57d0af7f1ada652ebddb3584a10f792569a5de
Josh <joshconstantinelim@gmail.com>
2023-12-27 19:28:55 +1100

    hide login button if already logged in

--- src/app/components/loginButton.tsx
+++ src/app/components/loginButton.tsx
@@ -3,8 +3,13 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
+import { useSession } from "next-auth/react";
+
 export default function LoginButton() {
   const pathname = usePathname();
+  const session = useSession();
+
+  if (session.status === "authenticated") return <></>;
 
   return <Link href={`/auth/login?from=${pathname}`}>Login</Link>;
 }
