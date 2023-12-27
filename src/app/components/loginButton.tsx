ef45f8a6d435fba249835522c3e00e97405ca302
Josh <joshconstantinelim@gmail.com>
2023-12-27 22:14:42 +1100

    login button refactor

--- src/app/components/loginButton.tsx
+++ src/app/components/loginButton.tsx
@@ -1,25 +1,19 @@
 "use client";
 
-import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 import { signOut, useSession } from "next-auth/react";
 
-const logout = async () => {
-  await signOut();
-};
-
 export default function LoginButton() {
   const pathname = usePathname();
   const session = useSession();
 
-  if (session.status === "authenticated")
-    return (
-      <Link href="" onClick={() => signOut()}>
-        Logout
-      </Link>
-    );
-
-  return <Link href={`/auth/login?from=${pathname}`}>Login</Link>;
+  return session.status === "authenticated" ? (
+    <Link href="" onClick={() => signOut()}>
+      Logout
+    </Link>
+  ) : (
+    <Link href={`/auth/login?from=${pathname}`}>Login</Link>
+  );
 }
