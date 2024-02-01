657b4ce18544ab41edaa026ca1e637f5e5f926a3
Josh <joshconstantinelim@gmail.com>
2024-02-01 13:29:28 +1100

    fyg 2024

--- src/app/first-year-guide/page.tsx
+++ src/app/first-year-guide/page.tsx
@@ -1,26 +1,42 @@
-import { FYG_2021_LINK, FYG_2022_LINK, FYG_2023_LINK } from "../utils";
+import {
+  FYG_2021_LINK,
+  FYG_2022_LINK,
+  FYG_2023_LINK,
+  FYG_2024_LINK,
+} from "../utils";
 
 export default function FirstYearGuide() {
   return (
     <main className="bg-white">
-      <header className="text-white p-12 bg-[#4799d1] flex flex-col gap-4">
+      <header className="flex flex-col gap-4 bg-[#4799d1] p-12 text-white">
         <h1 className="text-3xl font-semibold">First Year Guide</h1>
         <p>
-            This <b>DataSoc First Year Guide</b> will allow you to familiarise yourself with UNSW, as the transition to university can feel super daunting and confusing. In particular, if you are a data science student, you will also benefit from our overview of this flourishing degree, equipping you with the knowledge you need to get started!
+          This <b>DataSoc First Year Guide</b> will allow you to familiarise
+          yourself with UNSW, as the transition to university can feel super
+          daunting and confusing. In particular, if you are a data science
+          student, you will also benefit from our overview of this flourishing
+          degree, equipping you with the knowledge you need to get started!
         </p>
         <p>
-        Look inside for our top tips for wandering the campus, maximising your social skills, raising your WAM and padding your resumé! 
+          Look inside for our top tips for wandering the campus, maximising your
+          social skills, raising your WAM and padding your resumé!
         </p>
         <p>
-            If you are interested, you can also view our
-              <a href={FYG_2021_LINK}> <u>2021</u> </a>
-              and
-              <a href={FYG_2022_LINK}> <u>2022</u> </a>
-              First Year Guides.
+          If you are interested, you can also view our
+          <a href={FYG_2021_LINK}>
+            {" "}
+            <u>2021</u>{" "}
+          </a>
+          and
+          <a href={FYG_2022_LINK}>
+            {" "}
+            <u>2022</u>{" "}
+          </a>
+          First Year Guides.
         </p>
       </header>
-      <div className="flex items-center justify-center align-baseline p-6">
-        <iframe className="w-2/3 h-screen" src={FYG_2023_LINK}/>
+      <div className="flex items-center justify-center p-6 align-baseline">
+        <iframe className="h-screen w-2/3" src={FYG_2024_LINK} />
       </div>
     </main>
   );
--- src/app/utils.tsx
+++ src/app/utils.tsx
@@ -54,7 +54,9 @@ export const FYG_2021_LINK =
 export const FYG_2022_LINK =
   "https://drive.google.com/file/d/1tfqWGa1CUkUhNagho0dYeYMGXQscnke0/preview";
 export const FYG_2023_LINK =
-  "https://drive.google.com/file/d/1Fdf56Csia7Ea3HNPSttzRIb5Cfg6z7H5/preview";
+  "https://drive.google.com/file/d/1Fdf56Csia7Ea3HNPSttzRIb5Cfg6z7H5/preview"; // TODO: this is just the 2021 guide
+export const FYG_2024_LINK =
+  "https://drive.google.com/file/d/1D6m4pJouKCBALRmy2kAFpMcdnJwS6lyH/preview";
 
 export const CAREERS_GUIDE =
   "https://drive.google.com/file/d/1-y2Uv3YahnbM9O8Xp1knhRs2qTSDTBJ5/preview";
