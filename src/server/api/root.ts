e282176d37b7437fc722cbc4ea4ffcd956f07b83
Ishita Gupta <ishita@DESKTOP-5LB8R22.>
2025-02-09 20:10:25 +1100

    Removing blogs and jobs from the root

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -3,11 +3,11 @@ import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 import { TRPCError } from "@trpc/server";
 
 import { authRouter } from "./routers/auth";
-import { blogRouter } from "./routers/blogs";
+// import { blogRouter } from "./routers/blogs";
 import { companiesRouter } from "./routers/companies";
 import { coverPhotoRouter } from "./routers/coverphoto";
 import { eventRouter } from "./routers/events";
-import { jobsRouter } from "./routers/jobs";
+// import { jobsRouter } from "./routers/jobs";
 import { resourcesRouter } from "./routers/resources";
 import { sponsorshipsRouter } from "./routers/sponsorships";
 import { tagsRouter } from "./routers/tags";
@@ -21,11 +21,11 @@ export const appRouter = createTRPCRouter({
     throw new TRPCError({ code: "BAD_REQUEST", message: "Bad greeting" });
   }),
   auth: authRouter,
-  blogs: blogRouter,
+  // blogs: blogRouter,
   companies: companiesRouter,
   coverPhotos: coverPhotoRouter,
   events: eventRouter,
-  jobs: jobsRouter,
+  // jobs: jobsRouter,
   resources: resourcesRouter,
   sponsorships: sponsorshipsRouter,
   tags: tagsRouter,
